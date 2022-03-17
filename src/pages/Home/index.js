71a7cbc69a5cfe188ecfdfcd51ee743a2202d301
Jean Izepon <izeponjc@gmail.com>
2022-03-17 20:52:18 -0300

    adicionado styled components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,30 @@
 function Home() {
   return (
     <>
-      <h1>Hello World!</h1>
-      <h2>Movie API</h2>
+      <h1>Movies</h1>
+      <ul>
+        <li>
+          <img
+            src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
+            alt='Spider-man'
+          />
+          <span>Spider Man Homecoming</span>
+        </li>
+        <li>
+          <img
+            src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
+            alt='Spider-man'
+          />
+          <span>Spider Man Homecoming</span>
+        </li>
+        <li>
+          <img
+            src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
+            alt='Spider-man'
+          />
+          <span>Spider Man Homecoming</span>
+        </li>
+      </ul>
     </>
   );
 }
