f33be773e4658bff1a10f2466473a971415dcf01
Jean Izepon <izeponjc@gmail.com>
2022-03-17 21:12:59 -0300

    adicionado estilo nos cards

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,31 +1,75 @@
+import { Container } from './styles';
+
 function Home() {
   return (
-    <>
+    <Container>
       <h1>Movies</h1>
       <ul>
         <li>
-          <img
-            src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
-            alt='Spider-man'
-          />
+          <a href='#'>
+            <img
+              src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
+              alt='Spider-man'
+            />
+          </a>
           <span>Spider Man Homecoming</span>
         </li>
         <li>
-          <img
-            src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
-            alt='Spider-man'
-          />
+          <a href='#'>
+            <img
+              src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
+              alt='Spider-man'
+            />
+          </a>
           <span>Spider Man Homecoming</span>
         </li>
         <li>
-          <img
-            src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
-            alt='Spider-man'
-          />
+          <a href='#'>
+            <img
+              src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
+              alt='Spider-man'
+            />
+          </a>
+          <span>Spider Man Homecoming</span>
+        </li>
+        <li>
+          <a href='#'>
+            <img
+              src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
+              alt='Spider-man'
+            />
+          </a>
+          <span>Spider Man Homecoming</span>
+        </li>
+        <li>
+          <a href='#'>
+            <img
+              src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
+              alt='Spider-man'
+            />
+          </a>
+          <span>Spider Man Homecoming</span>
+        </li>
+        <li>
+          <a href='#'>
+            <img
+              src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
+              alt='Spider-man'
+            />
+          </a>
+          <span>Spider Man Homecoming</span>
+        </li>
+        <li>
+          <a href='#'>
+            <img
+              src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
+              alt='Spider-man'
+            />
+          </a>
           <span>Spider Man Homecoming</span>
         </li>
       </ul>
-    </>
+    </Container>
   );
 }
 
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  h1 {
+    text-align: center;
+    margin: 4rem 0;
+  }
+
+  ul {
+    list-style: none;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    column-gap: 3rem;
+    row-gap: 4rem;
+  }
+
+  li {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  img {
+    width: 200px;
+    height: auto;
+    border-radius: 1rem;
+    margin-bottom: 2rem;
+  }
+
+  span {
+    font-weight: bold;
+    font-size: 120%;
+  }
+
+  a {
+    transition: all 0.3s;
+  }
+
+  a:hover {
+    transform: scale(1.1);
+  }
+`;
