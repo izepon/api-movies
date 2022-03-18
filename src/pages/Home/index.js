0ecc83e98e0d417ff8d94ddb4d9a96a89c124fa3
Jean Izepon <izeponjc@gmail.com>
2022-03-17 21:29:42 -0300

    componentes e estilização dos dados mocados

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,74 +1,60 @@
-import { Container } from './styles';
+import { Container, MovieList, Movie } from './styles';
 
 function Home() {
+  const movies = [
+    {
+      id: 1,
+      title: 'Spider-man Homecoming',
+      image_url:
+        'https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg',
+    },
+    {
+      id: 2,
+      title: 'Spider-man Homecoming',
+      image_url:
+        'https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg',
+    },
+    {
+      id: 3,
+      title: 'Spider-man Homecoming',
+      image_url:
+        'https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg',
+    },
+    {
+      id: 4,
+      title: 'Spider-man Homecoming',
+      image_url:
+        'https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg',
+    },
+    {
+      id: 5,
+      title: 'Spider-man Homecoming',
+      image_url:
+        'https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg',
+    },
+    {
+      id: 6,
+      title: 'Spider-man Homecoming',
+      image_url:
+        'https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg',
+    },
+  ];
+
   return (
     <Container>
       <h1>Movies</h1>
-      <ul>
-        <li>
-          <a href='#'>
-            <img
-              src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
-              alt='Spider-man'
-            />
-          </a>
-          <span>Spider Man Homecoming</span>
-        </li>
-        <li>
-          <a href='#'>
-            <img
-              src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
-              alt='Spider-man'
-            />
-          </a>
-          <span>Spider Man Homecoming</span>
-        </li>
-        <li>
-          <a href='#'>
-            <img
-              src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
-              alt='Spider-man'
-            />
-          </a>
-          <span>Spider Man Homecoming</span>
-        </li>
-        <li>
-          <a href='#'>
-            <img
-              src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
-              alt='Spider-man'
-            />
-          </a>
-          <span>Spider Man Homecoming</span>
-        </li>
-        <li>
-          <a href='#'>
-            <img
-              src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
-              alt='Spider-man'
-            />
-          </a>
-          <span>Spider Man Homecoming</span>
-        </li>
-        <li>
-          <a href='#'>
-            <img
-              src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
-              alt='Spider-man'
-            />
-          </a>
-          <span>Spider Man Homecoming</span>
-        </li>
-        <li>
-          <a href='#'>
-            <img
-              src='https://images-na.ssl-images-amazon.com/images/I/81QpHLbctXL.jpg'
-              alt='Spider-man'
-            />
-          </a>
-          <span>Spider Man Homecoming</span>
-        </li>
-      </ul>
+      <MovieList>
+        {movies.map((movie) => {
+          return (
+            <Movie key={movie.id}>
+              <a href='#'>
+                <img src={movie.image_url} alt='Spider-man' />
+              </a>
+              <span>{movie.title}</span>
+            </Movie>
+          );
+        })}
+      </MovieList>
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -5,20 +5,26 @@ export const Container = styled.div`
     text-align: center;
     margin: 4rem 0;
   }
+`;
 
-  ul {
-    list-style: none;
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-    column-gap: 3rem;
-    row-gap: 4rem;
-  }
+export const MovieList = styled.ul`
+  list-style: none;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  column-gap: 3rem;
+  row-gap: 4rem;
 
   li {
     display: flex;
     flex-direction: column;
     align-items: center;
   }
+`;
+
+export const Movie = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
   img {
     width: 200px;
