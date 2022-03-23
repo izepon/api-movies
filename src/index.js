424f7a3e3c93796951ee2c9ce1fda593d2dc0ec7
Jean Izepon <izeponjc@gmail.com>
2022-03-23 19:47:50 -0300

    criado rotas e sinopse do filme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './global.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
+import Details from './pages/Details';
+import './global.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/details/:id' element={<Details />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,23 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Container } from './styles';
 import { Movie } from '../../components/Movie/style';
 import { MovieList } from '../../components/MovieList/style';
-import { useState, useEffect } from 'react';
 import { APIKey } from '../../config/key';
 
 function Home() {
+  const imagePath = 'https://image.tmdb.org/t/p/w500/';
+
   const [movies, setMovies] = useState([]);
-  const image_path = 'https://image.tmdb.org/t/p/w500';
 
   useEffect(() => {
     fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}&language=en-US&page=1`
+      `https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}&language=en-US`
     )
       .then((response) => response.json())
-      .then((data) => setMovies(data.results));
+      .then((data) => {
+        setMovies(data.results);
+      });
   }, []);
 
   return (
@@ -23,12 +27,12 @@ function Home() {
         {movies.map((movie) => {
           return (
             <Movie key={movie.id}>
-              <a href='#'>
+              <Link to={`/details/${movie.id}`}>
                 <img
-                  src={`${image_path}${movie.poster_path}`}
+                  src={`${imagePath}${movie.poster_path}`}
                   alt={movie.title}
                 />
-              </a>
+              </Link>
               <span>{movie.title}</span>
             </Movie>
           );
